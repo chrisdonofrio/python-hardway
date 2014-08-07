3b849b06f5a47223a6db8ff0f33df77e347c5c7e
Chris Donofrio <cdonofrio20@gmail.com>
2014-08-07 01:01:38 -0400

    Comment out lines 10-15 and run script again

--- ex15.py
+++ ex15.py
@@ -13,15 +13,19 @@ print "Here's your file %r:" % filename
 print txt.read()
 
 #Print statement asking for user input and assigning that input to new variable
-print "Type the filename again:"
-file_again = raw_input("> ")
+#print "Type the filename again:"
+#file_again = raw_input("> ")
 
 #Use open function to a return file object assigned to new variable txt_again
 #This time, the argument assigned to open function is user input
-txt_again = open(file_again)
+#txt_again = open(file_again)
 
 #Once again, use read function to return string containing all characters in the file
-print txt_again.read()
+#print txt_again.read()
 
 #Study Drills:
 #1. Commenting above each piece of code. Getting the hang of describing what my code actually does.
+#2. Get the hang of using Google. For instance, searching "Python [name of function]" will often return 
+#valuable information explaining what that function does.
+#3. Already have a good idea of what functions or methods do.
+#4. 
