68cc77788b2dfc37833ba1c53a229a0791bac516
Chris Donofrio <cdonofrio20@gmail.com>
2014-08-08 20:05:27 -0400

    Try own function

---
+++ ex19-2.py
@@ -0,0 +1,15 @@
+def jackson_family(michael, janet, laToya, jermaine):
+	print "What year was each Jackson born?"
+	print "Michael: %d" % michael
+	print "Janet: %d" % janet
+	print "laToya: %d" % laToya
+	print "Jermaine: %d \n" % jermaine
+	
+jackson_family(1958, 1966, 1956, 1954)
+
+michael_year = 1958
+janet_year = 1966
+laToya_year = 1956
+jermaine_year = 1954
+
+jackson_family(michael_year, janet_year, laToya_year, jermaine_year)
